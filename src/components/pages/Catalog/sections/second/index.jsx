01c8b45a4927924e71c8ced3eaa427d5a3968a19
Stark <serj.elessar@gmail.com>
2021-08-25 23:04:54 +0300

    Second section final

--- src/components/pages/Catalog/sections/second/index.jsx
+++ src/components/pages/Catalog/sections/second/index.jsx
@@ -11,7 +11,7 @@ const PromoForm = (props) => {
         <div className={promoForm.body}>
           <div className={promoForm.column}>
 
-            {itemsData.map(({ nameClass, title, description }) => <FeatureItem nameClass={nameClass} title={title} description={description} /> ) }
+            {itemsData.map(({ iconClass, title, description }) => <FeatureItem iconClass={iconClass} title={title} description={description} /> ) }
 
           </div>
           <div className={promoForm.column}>
@@ -23,11 +23,11 @@ const PromoForm = (props) => {
   )
 }
 
-const FeatureItem = ({ nameClass, title, description }) => {
+const FeatureItem = ({ iconClass, title, description }) => {
   return (
     <div className={promoForm.item}>
       <div className={promoForm.icon}>
-        <i class={nameClass}></i>
+        <i class={iconClass}></i>
       </div>
       <div className={promoForm.text}>
         <h3>{title}</h3>
@@ -38,9 +38,9 @@ const FeatureItem = ({ nameClass, title, description }) => {
 }
 
 const itemsData = [
-  { nameClass: "fas fa-american-sign-language-interpreting", title: "Официальный представитель.", description: "Все товары лучшие в своей ценовой категории. Официальные поставщики SHTENLI и PROFI в Беларуси." },
-  { nameClass: "fas fa-luggage-cart", title: "Доставка с любую точку Беларуси.", description: "Своя курьерская служба. Курьер заинтересован доставить товар в целосности." },
-  { nameClass: "fas fa-certificate", title: "Весь товар сертифицирован и имеет гарантию.", description: "Только официальные поставщики и производители товаров, такие как: Германия, Польша, Россия." },
+  { iconClass: "fas fa-american-sign-language-interpreting", title: "Официальный представитель.", description: "Все товары лучшие в своей ценовой категории. Официальные поставщики SHTENLI и PROFI в Беларуси." },
+  { iconClass: "fas fa-luggage-cart", title: "Доставка с любую точку Беларуси.", description: "Своя курьерская служба. Курьер заинтересован доставить товар в целосности." },
+  { iconClass: "fas fa-certificate", title: "Весь товар сертифицирован и имеет гарантию.", description: "Только официальные поставщики и производители товаров, такие как: Германия, Польша, Россия." },
 ]
 
 export default PromoForm;
