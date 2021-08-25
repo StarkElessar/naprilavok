bbb62cf695d972ad40196edf79e48ce27d8b16ac
Stark <serj.elessar@gmail.com>
2021-08-25 18:01:53 +0300

    chenge

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
-import Footer from './components/Footer';
-import Catalog from './components/Catalog';
-import Greenhouses from './components/Greenhouses';
-import Pavilion from './components/Pavilion';
-import Mowers from './components/Mowers';
-import Chainsaw from './components/Chainsaw';
-import WeldingMachine from './components/WaldingMachine';
-import Showers from './components/Showers';
-import PhoneLink from './components/mini-phone';
+import Footer from './components/common/Footer';
+import Catalog from './components/page/Catalog';
+import Greenhouses from './components/page/Greenhouses';
+import Pavilion from './components/page/Pavilion';
+import Mowers from './components/page/Mowers';
+import Chainsaw from './components/page/Chainsaw';
+import WeldingMachine from './components/page/WaldingMachine';
+import Showers from './components/page/Showers';
+import PhoneLink from './components/common/mini-phone';
 
 const App = () => {
   return (
---
+++ src/components/page/Pavilion/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import pavilionPage from './Pavilion.module.css';
+
+const Pavilion = (props) => {
+   return (
+      <div>
+         <h1 className={pavilionPage.title}>Беседки из поликарбоната от производителя</h1>
+      </div>
+   );
+};
+
+export default Pavilion;
