5d3c6e9600e4e04690ee346bae7dfaf94b240e06
Stark <serj.elessar@gmail.com>
2021-11-01 21:32:46 +0300

    Install Mask Input

--- src/components/common/collection-forms/main-form/index.jsx
+++ src/components/common/collection-forms/main-form/index.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import form from './form.module.css'
+import React, {useState} from 'react';
+import form from './form.module.css';
+import InputMask from 'react-input-mask';
 
 const MainForm = (props) => {
+  const [userName, setUserName] = useState('');
+  const [userPhone, setUserPhone] = useState('');
+  const onEmailChange = (event) => setUserName(event.target.value);
+  const onPhoneChange = (event) => setUserPhone(event.target.value);
+
+  const onSendData = (event) => {
+    event.preventDefault();
+  }
+
   return (
-    <form action="mail-telegram.php" name="main-form" method="POST">
+    <form action="#" name="main-form" method="GET">
       <input type="hidden" name="utm_source" value="" />
       <input type="hidden" name="utm_medium" value="" />
       <input type="hidden" name="utm_campaign" value="" />
@@ -12,12 +22,31 @@ const MainForm = (props) => {
       <h3>Заполните эту форму и получите консультацию в течении 5 минут</h3>
       <p>Заполните поля ниже. За вашим телефоном закрепляется доступ к ценам со скидкой.</p>
       <label for="user_name">
-        <input id="user_name" type="text" name="user_name" value="" placeholder="Ваше имя" required />
+        <InputMask
+          onChange={onEmailChange}
+          id="user_name"
+          type="text"
+          name="user_name"
+          value={userName}
+          placeholder="Ваше имя"
+          required
+        />
       </label>
       <label for="user_phone">
-        <input id="user_phone" type="tel" name="user_phone" value="" placeholder="+375 (" required />
+        <InputMask
+          mask='+375 (99) 999-99-99'
+          onChange={onPhoneChange}
+          value={userPhone}
+          id="user_phone"
+          type="tel"
+          name="user_phone"
+          placeholder="+375 ("
+          required
+        />
       </label>
-      <button type="submit" >Получить консультацию</button>
+      <button disabled={!userName || !userPhone}>
+        Получить консультацию
+      </button>
     </form>
   )
 }
